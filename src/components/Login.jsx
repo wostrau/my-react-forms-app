010d4c9457e4a6e775f160dda2f20f00ad0fee03
wostrau <a.ostrovtsov@gmail.com>
2024-03-17 23:29:44 +0100

    added FormData object for storing data from forms inputs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,72 @@
+import { useRef } from 'react';
+import { useState } from 'react';
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+
+  // const [enteredValue, setEnteredValue] = useState({ email: '', password: '' });
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log({
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    });
+  }
+
+  // function handleInputChange(e) {
+  //   setEnteredValue((prevState) => ({
+  //     ...prevState,
+  //     [e.target.name]: e.target.value,
+  //   }));
+  // }
+
+  // function handleEmailChange(e) {
+  //   setEnteredEmail(e.target.value);
+  // }
+
+  // function handlePasswordChange(e) {
+  //   setEnteredPassword(e.target.value);
+  // }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+      <div className='control-row'>
+        <div className='control no-margin'>
+          <label htmlFor='email'>Email</label>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            ref={emailRef}
+            // value={enteredValue.email}
+            // onChange={handleInputChange}
+          />
         </div>
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+        <div className='control no-margin'>
+          <label htmlFor='password'>Password</label>
+          <input
+            id='password'
+            type='password'
+            name='password'
+            ref={passwordRef}
+            // value={enteredValue.password}
+            // onChange={handleInputChange}
+          />
         </div>
       </div>
 
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+      <p className='form-actions'>
+        <button className='button button-flat'>Reset</button>
+        <button className='button'>Login</button>
       </p>
     </form>
   );
